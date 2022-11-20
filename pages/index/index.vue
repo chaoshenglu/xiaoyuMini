c0fe5af1f5cc57fc78247faf9a2f9bbb0cdb86cd
lixiang <271896700@qq.com>
2022-11-20 22:34:02 +0800

    1

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -4,6 +4,12 @@
       <view class="lxCenterColumn">
         <view class="listCell lxColumn" v-for="(tiezi,index) in tieziArr" :key="tiezi.id" @click="tapCell(tiezi)">
           <text class="title">{{tiezi.title}}</text>
+          <view class="lxCenterRow">
+            <uni-tag :text="tiezi.time" type="primary" size="small"></uni-tag>
+            <uni-tag text="标签" type="success" size="small"></uni-tag>
+            <uni-tag text="标签" type="warning" size="small"></uni-tag>
+            <uni-tag text="标签" type="error" size="small"></uni-tag>
+          </view>
           <text>{{tiezi.title}}</text>
           <text>{{tiezi.title}}</text>
           <text>{{tiezi.title}}</text>
@@ -23,6 +29,7 @@
   const queryList = (pageNo, pageSize) => {
     let uri = 'tiezi/getTieZiArr'
     getApp().get(uri).then(res => {
+      console.log(JSON.stringify(res.data[0], null, 2))
       paging.value.complete(res.data || [])
     }).catch(res => {
       paging.value.complete(false)
@@ -35,8 +42,16 @@
     background-color: #F6F6F6;
   }
 
+  .uni-tag--small {
+    border-radius: 4px !important;
+    margin-right: 4px;
+    padding-top: 0px !important;
+    padding-bottom: 0px !important;
+  }
+
   .title {
-    font-size: 15px;
+    font-size: 16px;
     color: #222222;
+    margin-top: 6px;
   }
 </style>
