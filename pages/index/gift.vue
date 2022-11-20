501904f41718c865e96a18bf5a37cdce796b0e2f
lixiang <271896700@qq.com>
2022-11-21 00:27:25 +0800

    1

---
+++ pages/index/gift.vue
@@ -0,0 +1,40 @@
+<template>
+  <view class="gift">
+
+  </view>
+</template>
+
+<script>
+  export default {
+    props: {
+      gift: {
+        type: Object,
+        default () {
+          return {
+
+          }
+        }
+      }
+    },
+
+    data() {
+      return {}
+    },
+
+    computed: {
+
+    },
+
+    methods: {
+
+    }
+  }
+</script>
+
+<style lang="scss">
+  .gift {
+    position: fixed;
+    bottom: 160rpx;
+    width: 100vw;
+  }
+</style>
