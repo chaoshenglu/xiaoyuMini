6e1b4f6008270228874fcdc50ee69c90ea330d8b
lixiang <271896700@qq.com>
2022-11-21 19:34:34 +0800

    1

--- pages/baoMing/baoMing.vue
+++ pages/baoMing/baoMing.vue
@@ -1,18 +1,19 @@
 <template>
-  <view class="pageView">
+  <view>
     <uni-popup ref="popup" type="center">
       <Gift @closePop="closePop" />
     </uni-popup>
-    <uni-grid :column="4" :highlight="true" @change="change" :showBorder="false">
-      <uni-grid-item v-for="(person, index) in personArr" :index="index" :key="index">
-        <view class="grid-item-box" style="background-color: #fff;">
-          <image :src="person.avatar" mode="aspectFit" style="width: 30px;height: 30px;"></image>
-          <text class="text">{{person.nickName}}</text>
-        </view>
-      </uni-grid-item>
-    </uni-grid>
-
-
+    <view style="margin-left: 8px;margin-right: 8px;margin-top: 8px;">
+      <uni-grid :column="4" :highlight="true" @change="change" :showBorder="false" :square="false">
+        <uni-grid-item v-for="(person, index) in personArr" :index="index" :key="index">
+          <view class="lxCenterR cell" :class="{ 'boyClass': person.isBoy === 1}">
+            <image class="head" :src="person.avatar" mode="aspectFit"></image>
+            <text v-if="person.nickName.length < 3" class="pname">{{person.nickName}}</text>
+            <text v-else class="pname">{{person.nickName.slice(0,2)}}</text>
+          </view>
+        </uni-grid-item>
+      </uni-grid>
+    </view>
 
 
   </view>
@@ -62,5 +63,27 @@
   .pageView {
     width: 100vw;
     height: 100vh;
+  }
+
+  .pname {
+    margin-left: 6px;
+    color: white;
+  }
+
+  .boyClass {
+    background-color: #4685F3 !important;
+  }
+
+  .cell {
+    background-color: #FD5FA9;
+    border-radius: 6px;
+    margin: 5px;
+    padding: 5px;
+  }
+
+  .head {
+    width: 26px;
+    height: 26px;
+    border-radius: 13px;
   }
 </style>
