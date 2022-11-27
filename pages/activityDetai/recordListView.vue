3c4e7042d71bd914f40ff9446c2951fa95149190
lixiang <271896700@qq.com>
2022-11-28 01:01:14 +0800

    1

---
+++ pages/activityDetai/recordListView.vue
@@ -0,0 +1,36 @@
+<template>
+  <view class="lxColumn" style="margin-top: 10px;">
+
+  </view>
+</template>
+
+<script setup>
+  import {
+    computed,
+    onMounted,
+    ref
+  } from 'vue'
+  const props = defineProps(['tiezi'])
+  const tz = computed(() => {
+    return props.tiezi
+  })
+  let recordArr = ref([])
+
+  onMounted(() => {
+    getRecordArr()
+  })
+
+  function getRecordArr() {
+    let param = {}
+    param.tieziId = tz.value.id
+    getApp().get('tz_record/getTZRecord', param).then(res => {
+      recordArr.value = res.data || []
+    }).catch(err => {
+      console.log(err)
+    })
+  }
+</script>
+
+<style lang="scss">
+
+</style>
