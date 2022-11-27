2ecab8d7d0543dfc6d6469839461eae3a93ed7c1
lixiang <271896700@qq.com>
2022-11-27 20:44:43 +0800

    1

--- pages/activityDetai/activityDetai.vue
+++ pages/activityDetai/activityDetai.vue
@@ -1,18 +1,22 @@
 <template>
   <view>
-    <lgd-tab v-model="tabIndex" :tabValue="tabs" underlineColor="#4685F3" fontSize="14" background="#f6f6f6" />
+
   </view>
 </template>
 
-<script setup>
-  import {
-    computed,
-    ref
-  } from 'vue'
-  let tabs = ref(['活动信息', '报名人员', '操作记录', '活动费用'])
-  let tabIndex = ref(0)
+<script>
+  export default {
+    data() {
+      return {
+
+      }
+    },
+    methods: {
+
+    }
+  }
 </script>
 
-<style lang="scss">
+<style>
 
 </style>
