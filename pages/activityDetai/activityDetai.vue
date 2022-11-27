c113e23fbc60449166d8260b94a254460050f7d3
lixiang <271896700@qq.com>
2022-11-27 20:46:37 +0800

    1

--- pages/activityDetai/activityDetai.vue
+++ pages/activityDetai/activityDetai.vue
@@ -1,6 +1,6 @@
 <template>
   <view>
-
+    <lgd-tab v-model="tabIndex" :tabValue="tabs" underlineColor="#4685F3" fontSize="14" @change="didChangeTabIndex" />
   </view>
 </template>
 
@@ -8,7 +8,8 @@
   export default {
     data() {
       return {
-
+        tabs: ['活动信息', '报名人员', '操作记录', '活动费用'],
+        tabIndex: 0
       }
     },
     methods: {
@@ -17,6 +18,6 @@
   }
 </script>
 
-<style>
+<style lang="scss">
 
 </style>
