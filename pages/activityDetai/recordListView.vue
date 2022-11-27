360cebf8303e4f44449ee2290e7ac27d7145e186
lixiang <271896700@qq.com>
2022-11-28 01:36:35 +0800

    1

--- pages/activityDetai/recordListView.vue
+++ pages/activityDetai/recordListView.vue
@@ -24,10 +24,21 @@
   function getRecordArr() {
     let param = {}
     param.tieziId = tz.value.id
+    // actionType 1报名 2为加一报名 3为自己取消报名 4为自己的加一取消报名 5为其他人取消报名
     getApp().get('tz_record/getTZRecord', param).then(res => {
       let oriArr = res.data || []
       for (const item of oriArr) {
-        item.title = '这是标题啊'
+        if (item.actionType === 1) {
+          item.title = item.nickName + '报名'
+        } else if (item.actionType === 2) {
+          item.title = item.nickName + `报名+1（${item.onNickName})`
+        } else if (item.actionType === 3) {
+          item.title = item.nickName + '取消了报名'
+        } else if (item.actionType === 4) {
+          item.title = item.nickName + `取消了+1（${item.onNickName}）`
+        } else if (item.actionType === 5) {
+          item.title = item.nickName + `将${item.onNickName}从帖子中移除`
+        }
         item.desc = item.createTime
       }
       recordArr.value = oriArr
