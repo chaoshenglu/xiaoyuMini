9cbb122508b5c91c1550e168848333a21e7c8a6c
lixiang <271896700@qq.com>
2022-11-30 20:46:25 +0800

    1

--- pages/jifenHome/jifenHome.vue
+++ pages/jifenHome/jifenHome.vue
@@ -1,6 +1,6 @@
 <template>
   <view>
-
+    <image class="bg" src="/static/jifenBg.png" mode="aspectFill"></image>
   </view>
 </template>
 
@@ -20,5 +20,10 @@
 </script>
 
 <style lang="scss">
-
+  .bg {
+    width: 100vw;
+    height: 66.67vw;
+    position: fixed;
+    top: 0px;
+  }
 </style>
