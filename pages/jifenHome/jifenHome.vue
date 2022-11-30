d2940d5796e88ff7d5b829522a920afc88e25474
lixiang <271896700@qq.com>
2022-11-30 21:15:48 +0800

    q

--- pages/jifenHome/jifenHome.vue
+++ pages/jifenHome/jifenHome.vue
@@ -1,6 +1,18 @@
 <template>
   <view>
+
     <image class="bg" src="/static/jifenBg.png" mode="aspectFill"></image>
+    <view class="lxCenterRow jifenAndSignBtn">
+      <view class="lxColumn" style="width: 100px;">
+        <text style="color: white;font-size: 30px;font-weight: bold;">{{jifen}}</text>
+        <view class="lxCenterRow">
+          <uni-icons type="help" color="#fff"></uni-icons>
+          <text style="color: white;font-size: 11px;margin-left: 2px;">积分说明</text>
+        </view>
+      </view>
+
+      <view class="lxCenterC btn">已签到</view>
+    </view>
   </view>
 </template>
 
@@ -9,13 +21,14 @@
     computed,
     ref
   } from 'vue'
+  let jifen = ref(0)
 
   import {
     onLoad
   } from "@dcloudio/uni-app"
 
   onLoad((option) => {
-
+    jifen.value = getApp().globalData.user.jifen || 0
   })
 </script>
 
@@ -25,5 +38,23 @@
     height: 66.67vw;
     position: fixed;
     top: 0px;
+    z-index: -1;
+  }
+
+  .btn {
+    background-color: white;
+    border-radius: 17px;
+    width: 80px;
+    height: 34px;
+    color: #999999;
+    font-size: 14px;
+  }
+
+  .jifenAndSignBtn {
+    margin-left: 30px;
+    margin-right: 26px;
+    margin-top: 10px;
+    margin-bottom: 10px;
+    justify-content: space-between;
   }
 </style>
