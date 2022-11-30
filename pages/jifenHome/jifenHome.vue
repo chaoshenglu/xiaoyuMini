07adddadb18d60318280f1027aef0c70fda0ed15
lixiang <271896700@qq.com>
2022-11-30 21:40:33 +0800

    1

--- pages/jifenHome/jifenHome.vue
+++ pages/jifenHome/jifenHome.vue
@@ -1,6 +1,5 @@
 <template>
   <view>
-
     <image class="bg" src="/static/jifenBg.png" mode="aspectFill"></image>
     <view class="lxCenterRow jifenAndSignBtn">
       <view class="lxColumn" style="width: 100px;">
@@ -10,8 +9,23 @@
           <text style="color: white;font-size: 11px;margin-left: 2px;">积分说明</text>
         </view>
       </view>
-
       <view class="lxCenterC btn">已签到</view>
+    </view>
+    <view class="card lxColumn">
+      <view class="lxCenterRow">
+        <image src="/static/choujiang.png" style="width: 30px;height: 50px;" mode="aspectFit"></image>
+        <view class="lxColumn">
+          <text>天天抽奖</text>
+          <text>最高得10元立减金</text>
+        </view>
+      </view>
+      <view class="lxCenterRow">
+        <image src="/static/duihuan.png" style="width: 44px;height: 44px;" mode="aspectFit"></image>
+        <view class="lxColumn">
+          <text>积分兑换优惠券</text>
+          <text>10积分价值1元</text>
+        </view>
+      </view>
     </view>
   </view>
 </template>
@@ -33,12 +47,25 @@
 </script>
 
 <style lang="scss">
+  page {
+    background-color: #f6f6f6;
+  }
+
   .bg {
     width: 100vw;
     height: 66.67vw;
     position: fixed;
     top: 0px;
     z-index: -1;
+  }
+
+  .card {
+    background-color: white;
+    border-radius: 10px;
+    width: 92vw;
+    margin-left: 4vw;
+    height: 170px;
+    box-shadow: 0px 0px 4px 4px rgba(0, 0, 0, 0.02);
   }
 
   .btn {
