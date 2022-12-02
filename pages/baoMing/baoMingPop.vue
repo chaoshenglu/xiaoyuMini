328c6c16a3118dbe65f2d9c820eee6a0f917cd23
lixiang <271896700@qq.com>
2022-12-02 15:07:22 +0800

    1

--- pages/baoMing/baoMingPop.vue
+++ pages/baoMing/baoMingPop.vue
@@ -55,7 +55,7 @@
 
   function tapConfirm() {
     let user = getApp().globalData.user
-    user.isGirl = current.value
+    user.isGirl = parseInt(current.value)
     user.isJiaYi = 0
     user.status = 1 //1已报名2已取消3已飞机
     addTZRecord(user, props.tiezi.id)
