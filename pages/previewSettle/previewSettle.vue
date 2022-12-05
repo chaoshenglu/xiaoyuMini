7260f76ef9614b963e072dbc0649dabe0da1a1e4
lixiang <271896700@qq.com>
2022-12-05 11:01:55 +0800

    1

--- pages/previewSettle/previewSettle.vue
+++ pages/previewSettle/previewSettle.vue
@@ -3,13 +3,12 @@
     <uni-section title="执行参数" type="line" padding="0">
       <view class="lxColumn" style="margin-left: 20px;">
         <view class="lxCenterRow">
-          <view class="lxColumn">
+          <view class="lxColumn" style="width: 150px;">
             <text class="lx999">场地时长：2小时</text>
             <text class="lx999">场地单价：40元/小时</text>
             <text class="lx999">场地数量：1</text>
             <text class="lx999">用球数量：6个</text>
             <text class="lx999">用球单价：8元/个</text>
-
           </view>
           <view class="lxColumn" style="margin-left: 30px;">
             <text class="lx999">男生人数：5</text>
@@ -19,23 +18,30 @@
             <text class="lx999">正义指数：0.7</text>
           </view>
         </view>
-
       </view>
     </uni-section>
 
     <uni-section title="计算结果" type="line" padding="0">
       <view class="lxColumn" style="margin-left: 20px;">
         <view class="lxCenterRow">
-          <view class="lxColumn">
-            <text class="lx999">男生费用：15元/人</text>
-            <text class="lx999">女生费用：11元/人</text>
-            <text class="lx999">飞机费用：10元/人</text>
+          <view class="lxColumn" style="width: 150px;">
+            <text class="lx999">男生：15元/人</text>
+            <text class="lx999">女生：11元/人</text>
+            <text class="lx999">飞机：10元/人</text>
           </view>
           <view class="lxColumn" style="margin-left: 30px;">
-            <text class="lx999">男生VIP费用：14元/人</text>
-            <text class="lx999">女生VIP费用：10元/人</text>
+            <text class="lx999">男生VIP：13元/人</text>
+            <text class="lx999">女生VIP：9元/人</text>
             <text class="lx999">总费用：139元</text>
           </view>
+        </view>
+      </view>
+    </uni-section>
+
+    <uni-section title="费用分摊" type="line" padding="0">
+      <view class="lxColumn" style="margin-left: 20px;">
+        <view class="lxColumn" v-for="(person, index) in personArr" :index="index" :key="index">
+          <text class="lx999">{{person.nickName}}【{{person.isGirl === 1 ? '女':'男'}}】</text>
         </view>
       </view>
     </uni-section>
@@ -52,6 +58,13 @@
   import {
     onLoad
   } from "@dcloudio/uni-app"
+
+  const personArr = [{
+    nickName: '张三',
+    isGirl: 0,
+    isVip: 0,
+    money: 10
+  }]
 
   onLoad((option) => {
 
