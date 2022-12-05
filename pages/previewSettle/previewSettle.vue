7d86ee53db108e336f83e8e2b675e08c36a00feb
lixiang <271896700@qq.com>
2022-12-05 14:16:09 +0800

    1

--- pages/previewSettle/previewSettle.vue
+++ pages/previewSettle/previewSettle.vue
@@ -4,20 +4,20 @@
       <view class="lxColumn" style="margin-left: 20px;">
         <view class="lxCenterRow">
           <view class="lxColumn" style="width: 160px;">
-            <text class="lx999">场地时长：2小时</text>
-            <text class="lx999">场地单价：40元/小时</text>
-            <text class="lx999">场地数量：1</text>
-            <text class="lx999">用球数量：6个</text>
-            <text class="lx999">用球单价：8元/个</text>
-            <text class="lx999">手动改价：0元</text>
+            <text class="lx999">场地时长：{{parseInt(settle.fieldHours)}}小时</text>
+            <text class="lx999">场地单价：{{parseInt(settle.fieldPrice)}}元/小时</text>
+            <text class="lx999">场地数量：{{parseInt(settle.fieldNum)}}</text>
+            <text class="lx999">用球数量：{{parseInt(settle.ballNum)}}个</text>
+            <text class="lx999">用球单价：{{settle.ballPrice}}元/个</text>
+            <text class="lx999">手动改价：{{settle.gaiJia}}元</text>
           </view>
           <view class="lxColumn" style="margin-left: 30px;">
-            <text class="lx999">男生人数：5</text>
-            <text class="lx999">女生人数：2</text>
-            <text class="lx999">VIP男生人数：0</text>
-            <text class="lx999">VIP女生人数：0</text>
-            <text class="lx999">飞机人数：3</text>
-            <text class="lx999">正义指数：0.7</text>
+            <text class="lx999">男生人数：{{parseInt(settle.boyNum)}}</text>
+            <text class="lx999">女生人数：{{parseInt(settle.girlNum)}}</text>
+            <text class="lx999">VIP男生人数：{{parseInt(settle.boyVipNum)}}</text>
+            <text class="lx999">VIP女生人数：{{parseInt(settle.girlVipNum)}}</text>
+            <text class="lx999">飞机人数：{{parseInt(settle.flyNum)}}</text>
+            <text class="lx999">正义指数：{{settle.justice}}</text>
           </view>
         </view>
       </view>
@@ -76,7 +76,7 @@
   })
 
   function backEdit() {
-
+    uni.navigateBack()
   }
 
   function confirm() {
