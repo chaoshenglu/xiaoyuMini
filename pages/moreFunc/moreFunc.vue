4c3928b80a37b77875625f923b42ef095f065a36
lixiang <271896700@qq.com>
2022-12-06 09:48:48 +0800

    1

---
+++ pages/moreFunc/moreFunc.vue
@@ -0,0 +1,24 @@
+<template>
+  <view>
+
+  </view>
+</template>
+
+<script setup>
+  import {
+    computed,
+    ref
+  } from 'vue'
+
+  import {
+    onLoad
+  } from "@dcloudio/uni-app"
+
+  onLoad((option) => {
+
+  })
+</script>
+
+<style lang="scss">
+
+</style>
