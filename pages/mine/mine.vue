0cce97a12d4b148e9095455d7211e177cc4d78bc
lixiang <271896700@qq.com>
2022-12-06 09:41:02 +0800

    1

--- pages/mine/mine.vue
+++ pages/mine/mine.vue
@@ -19,7 +19,7 @@
 
     <MineCell title="我的积分" icon="/static/jifen.png" desc="100" @tapMineCell="tapMineCell" />
     <MineCell title="我的优惠券" icon="/static/coupon.png" @tapMineCell="tapMineCell" />
-    <MineCell title="我的报名" icon="/static/baoming.png" @tapMineCell="tapMineCell" />
+    <MineCell title="我的活动" icon="/static/baoming.png" @tapMineCell="tapMineCell" />
     <MineCell title="我的组织" icon="/static/club.png" @tapMineCell="tapMineCell" />
     <MineCell title="更多功能" icon="/static/moreFunc.png" @tapMineCell="tapMineCell" />
     <MineCell title="清除缓存" icon="/static/clear.png" @tapMineCell="tapMineCell" />
@@ -49,7 +49,7 @@
       uni.navigateTo({
         url: '/pages/couponList/couponList'
       })
-    } else if (e === '我的报名') {
+    } else if (e === '我的活动') { //我发起的 我参与的
 
     } else if (e === '我的组织') {
 
