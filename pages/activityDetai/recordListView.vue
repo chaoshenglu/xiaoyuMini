a30ccceaa3ccde80b25d3f6b3ace92c6d4b3f73b
lixiang <271896700@qq.com>
2022-12-12 10:06:49 +0800

    1

--- pages/activityDetai/recordListView.vue
+++ pages/activityDetai/recordListView.vue
@@ -4,8 +4,12 @@
       <view class="lxCenterColumn" v-for="(record, index) in recordArr" :index="index" :key="index">
         <view class="lxCenterRow listCell" :class="{ firstClass: index===0 }" style="justify-content: space-between;"
           @click="tapCell(record)">
-          <text class="lx333" style="font-size: 14px;">{{record.title}}</text>
-          <text class="lx999" style="font-size: 14px;">{{record.createTime}}</text>
+          <view v-if="record.qiuguanTinyName" class="lxCenterRow">
+            <text class="lx333" style="font-size: 14px;">{{record.title}}</text>
+            <text class="lx999" style="font-size: 12px;margin-left: 3px;">[{{record.qiuguanTinyName}}]</text>
+          </view>
+          <text v-else class="lx333" style="font-size: 14px;">{{record.title}}</text>
+          <text class="lx999" style="font-size: 14px;">{{record.createTime.slice(0,16)}}</text>
         </view>
       </view>
     </z-paging>
@@ -20,6 +24,13 @@
   const props = defineProps(['tiezi'])
   const tz = computed(() => {
     return props.tiezi
+  })
+  const qiuguanArr = computed(() => {
+    if (props.tiezi.qiuguanArr) {
+      return JSON.parse(props.tiezi.qiuguanArr)
+    } else {
+      return []
+    }
   })
   let recordArr = ref([])
   const paging = ref(null)
@@ -36,6 +47,13 @@
     getApp().get('tz_record/getTZRecord', param).then(res => {
       let arr = res.data.list || []
       for (const item of arr) {
+
+        for (const qiuguan of qiuguanArr.value) {
+          if (item.qiuguanId === qiuguan.qiuguanId) {
+            item.qiuguanTinyName = qiuguan.qiuguanTinyName
+          }
+        }
+
         if (item.actionType === 1) {
           item.title = item.nickName + '报名'
         } else if (item.actionType === 2) {
