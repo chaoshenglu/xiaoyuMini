40f71cbc0da599fd3a8449d766dd0e1f3379cc68
lixiang <271896700@qq.com>
2022-12-13 23:33:45 +0800

    1

--- pages/privacy/privacy.vue
+++ pages/privacy/privacy.vue
@@ -1,17 +1,18 @@
 <template>
-  <view class="lxColumn" style="padding-left:15px;padding-top: 10px;">
+  <view class="lxColumn" style="padding-left:15px;padding-top: 10px;padding-right: 3px;">
     <view class="lxCenterRow" style="justify-content: space-between;">
       <text class="lx333">保留我的报名记录</text>
-      <switch :checked="checkedBM===1" color="#4685F3" style="transform:scale(0.7)" />
+      <switch @change="changeBM" :checked="checkedBM===1" color="#4685F3" style="transform:scale(0.7)" />
     </view>
     <view class="lx100vwLine" style="margin-top: 10px;margin-bottom: 10px;" />
     <view class="lxCenterRow" style="justify-content: space-between;">
       <text class="lx333">保留我的活动记录</text>
-      <switch :checked="checkedHD===1" color="#4685F3" style="transform:scale(0.7)" />
+      <switch @change="changeHD" :checked="checkedHD===1" color="#4685F3" style="transform:scale(0.7)" />
     </view>
     <view class="lx100vwLine" style="margin-top: 10px;margin-bottom: 10px;" />
     <text class="lx999"
-      style="font-size: 13px;">注意，小程序所记录的特征码/头像/昵称并不会与微信号/手机号强关联，通常情况下，用户使用本程序，并不必担心隐私问题。若依然不想把个人的报名记录/活动记录保存在服务器，可取消勾选以上选项，并点击“保存”。</text>
+      style="font-size: 13px;margin-right: 18px;">注意，小程序所记录的头像/昵称并不会与微信号强关联，用户使用本程序，并不必担心隐私问题。若仍不想把个人的报名记录/活动记录保存在服务器，可取消勾选以上选项，并点击“保存”。</text>
+    <LXBottomBtn title="保存" @tapBottomBtn="save" />
   </view>
 </template>
 
@@ -24,14 +25,66 @@
     onLoad
   } from "@dcloudio/uni-app"
 
-  const checkedBM = ref(1)
-  const checkedHD = ref(1)
+  const checkedBM = ref(0)
+  const checkedHD = ref(0)
 
   onLoad((option) => {
-
+    let user = getApp().globalData.user
+    checkedBM.value = user.bmRecord
+    checkedHD.value = user.hdRecord
   })
+
+  function changeBM(e) {
+    console.log(e)
+    checkedBM.value = e.detail.value ? 1 : 0
+  }
+
+  function changeHD(e) {
+    checkedHD.value = e.detail.value ? 1 : 0
+  }
+
+  function save() {
+    console.log(checkedBM.value, checkedHD.value)
+    let user = getApp().globalData.user
+    let param = {
+      openid: user.openid,
+      bmRecord: checkedBM.value,
+      hdRecord: checkedHD.value
+    }
+    getApp().post('user/updateUserInfo', param).then(res => {
+      if (res.data === 1) {
+        user.bmRecord = checkedBM.value
+        user.hdRecord = checkedHD.value
+        uni.setStorageSync('user', user)
+        let content = null
+        if (checkedBM.value === 0 && checkedHD.value === 0) {
+          content = '您的报名记录和活动记录将在活动结束后24小时内从服务器上自动删除'
+        } else if (checkedBM.value === 0) {
+          content = '您的报名记录将在活动结束后24小时内从服务器上自动删除'
+        } else if (checkedHD.value === 0) {
+          content = '您的活动记录将在活动结束后24小时内从服务器上自动删除'
+        }
+        alert(content)
+      } else {
+        getApp().toastAndConsoleSystemError(res)
+      }
+    }).catch(err => {
+      getApp().toastAndConsoleSystemError(err)
+    })
+  }
+
+  function alert(content) {
+    if (content) {
+      uni.showModal({
+        title: '保存成功',
+        content: content,
+        showCancel: false,
+        success: res => {
+          uni.navigateBack()
+        }
+      })
+    } else {
+      uni.navigateBack()
+    }
+  }
 </script>
-
-<style lang="scss">
-
-</style>
