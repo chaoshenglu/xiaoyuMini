bcdcef67a7bcd183bd4bd677cbd947bd9743e291
lixiang <271896700@qq.com>
2022-12-13 21:01:08 +0800

    1

--- pages/privacy/privacy.vue
+++ pages/privacy/privacy.vue
@@ -1,18 +1,17 @@
 <template>
-  <view class="lxColumn">
-    <view class="lxCenterRow">
-      <text>保留我的报名记录</text>
-      <switch :checked="checkedBM===1" color="#4685F3" />
+  <view class="lxColumn" style="padding-left:15px;padding-top: 10px;">
+    <view class="lxCenterRow" style="justify-content: space-between;">
+      <text class="lx333">保留我的报名记录</text>
+      <switch :checked="checkedBM===1" color="#4685F3" style="transform:scale(0.7)" />
     </view>
-
-
-    <uni-card title="基础卡片" sub-title="副标题" @click="onClick">
-      <view class="lxCenterRow">
-        <text>保留我的活动记录</text>
-        <switch :checked="checkedHD===1" color="#4685F3" />
-      </view>
-    </uni-card>
-
+    <view class="lx100vwLine" style="margin-top: 10px;margin-bottom: 10px;" />
+    <view class="lxCenterRow" style="justify-content: space-between;">
+      <text class="lx333">保留我的活动记录</text>
+      <switch :checked="checkedHD===1" color="#4685F3" style="transform:scale(0.7)" />
+    </view>
+    <view class="lx100vwLine" style="margin-top: 10px;margin-bottom: 10px;" />
+    <text class="lx999"
+      style="font-size: 13px;">注意，小程序所记录的特征码/头像/昵称并不会与微信号/手机号强关联，通常情况下，用户使用本程序，并不必担心隐私问题。若依然不想把个人的报名记录/活动记录保存在服务器，可取消勾选以上选项，并点击“保存”。</text>
   </view>
 </template>
 
