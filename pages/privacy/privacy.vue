ad0f138de45796b0a57adddc938eff3374f435d0
lixiang <271896700@qq.com>
2022-12-13 20:50:45 +0800

    1

--- pages/privacy/privacy.vue
+++ pages/privacy/privacy.vue
@@ -1,5 +1,17 @@
 <template>
-  <view>
+  <view class="lxColumn">
+    <view class="lxCenterRow">
+      <text>保留我的报名记录</text>
+      <switch :checked="checkedBM===1" color="#4685F3" />
+    </view>
+
+
+    <uni-card title="基础卡片" sub-title="副标题" @click="onClick">
+      <view class="lxCenterRow">
+        <text>保留我的活动记录</text>
+        <switch :checked="checkedHD===1" color="#4685F3" />
+      </view>
+    </uni-card>
 
   </view>
 </template>
@@ -12,6 +24,9 @@
   import {
     onLoad
   } from "@dcloudio/uni-app"
+
+  const checkedBM = ref(1)
+  const checkedHD = ref(1)
 
   onLoad((option) => {
 
