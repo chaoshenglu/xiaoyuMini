cb7c48d92d4ab7bd280df26304b958b4fb5c7e57
lixiang <271896700@qq.com>
2022-12-26 20:02:35 +0800

    1

--- pages/mine/mine.vue
+++ pages/mine/mine.vue
@@ -19,9 +19,9 @@
 
     <MineCell title="我的积分" icon="/static/jifen.png" desc="100" @tapMineCell="tapMineCell" />
     <MineCell title="我的优惠券" icon="/static/coupon.png" @tapMineCell="tapMineCell" />
-    <!-- <MineCell title="我的活动" icon="/static/baoming.png" @tapMineCell="tapMineCell" /> -->
-    <!-- <MineCell title="我的组织" icon="/static/club.png" @tapMineCell="tapMineCell" /> -->
-    <!-- <MineCell title="更多功能" icon="/static/moreFunc.png" @tapMineCell="tapMineCell" /> -->
+    <MineCell title="我的活动" icon="/static/baoming.png" @tapMineCell="tapMineCell" />
+    <MineCell title="我的组织" icon="/static/club.png" @tapMineCell="tapMineCell" />
+    <MineCell title="更多功能" icon="/static/moreFunc.png" @tapMineCell="tapMineCell" />
     <MineCell title="清除缓存" icon="/static/clear.png" @tapMineCell="tapMineCell" />
     <MineCell title="隐私设置" icon="/static/privacy.png" @tapMineCell="tapMineCell" />
 
