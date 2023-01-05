05d3e85d9b9e15e98ea6316f62858194032b1c5d
lixiang <271896700@qq.com>
2023-01-05 17:17:16 +0800

    1

--- pages/myTiezi/myTiezi.vue
+++ pages/myTiezi/myTiezi.vue
@@ -1,9 +1,9 @@
 <template>
   <view>
     <z-paging ref="paging" v-model="tieziArr" @query="queryList">
-      <view class="lxCenterColumn">
-        <view class="listCell lxColumn" v-for="(tiezi,index) in tieziArr" :key="tiezi.id">
-          <view class="lxColumn" @click="tapCell(tiezi)" @longpress="didLongPress(tiezi)">
+      <view class="lxColumn">
+        <view class="myTieziCell lxColumn" v-for="(tiezi,index) in tieziArr" :key="tiezi.id">
+          <view class="lxColumn" @click="tapCell(tiezi)">
             <text class="title">{{tiezi.title}}</text>
             <view class="lxCenterRow" style="margin-top: 3px;margin-bottom: 9px;">
               <uni-tag :text="tiezi.shortQiuGuanName" type="error" size="small"></uni-tag>
@@ -12,6 +12,7 @@
               <uni-tag :text="tiezi.limitStr" type="warning" size="small"></uni-tag>
             </view>
             <text class="remark">备注：{{tiezi.remark || defaultRemark}}</text>
+            <view v-if="index < tieziArr.length-1" class="lx100vwLine" style="margin-top: 15px;" />
           </view>
         </view>
       </view>
@@ -29,6 +30,7 @@
     onLoad
   } from "@dcloudio/uni-app"
 
+  //lxtodo:测试翻页
   const paging = ref(null)
   const tieziArr = ref([])
   const defaultRemark = '请各位球友根据自身的身体情况控制运动强度，避免出现意外，如在运动中碰伤或突发疾病，组织者不承担任何法律责任。'
@@ -53,11 +55,19 @@
       paging.value.complete(false)
     })
   }
+
+  function tapCell(tiezi) {
+
+  }
 </script>
 
 <style lang="scss">
-  page {
-    background-color: #F6F6F6;
+  .myTieziCell {
+    padding-left: 18px;
+    padding-right: 18px;
+    padding-top: 10px;
+    margin-top: 5px;
+    position: relative;
   }
 
   .remark {
